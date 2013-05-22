61fe6ec1815782ac343f5d057442349a84091cce
Jailany <jailany@stellarteam.in>
2013-05-22 17:56:20 +0530

    Signed-off-by: Jailany <jailany@stellarteam.in>

---
+++ protected/views/message/exit.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this MessageController */
+
+$this->breadcrumbs=array(
+	'Message'=>array('/message'),
+	'Exit',
+);
+?>
+<h1>Exit page</h1>
+
+<p>
+	It is sad that you are leaving the <b><?php echo $this->action->id;?></b> page</p> 
+<h2><?php echo $value;?></h2>
---
+++ protected/views/message/index.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $this MessageController */
+
+$this->breadcrumbs=array(
+	'Message',
+);
+?>
+<h1>Welcome to the new site</h1>
+
+<p><b>Controller name:</b><?php echo $this->id;?></p>
+<p><b>action name:</b><?php echo $this->action->id;?></p> 
+
+<h3><a href="http://test.bane.com/index.php?r=message/exit">Go to the exit page</a></h3>
+
+<h3><?php echo CHtml::link('Click Here', array('message/exit'));?></h3>
+
